dbad6b7d8a5482947e92bc66824fcf03d67bef3a
benj-bernardi <benjaminbernardi33@gmail.com>
2025-10-21 20:45:35 -0300

    Update Node.js calculator script

--- mini-projects/project_calculator.js
+++ mini-projects/project_calculator.js
@@ -1,26 +1,82 @@
-function calculator(num1, num2, opr) {
-    switch (opr) {
-        case "+":
-            return `${num1} + ${num2} = ${num1 + num2}`;
+const readline = require("readline");
 
-        case "-":
-            return `${num1} - ${num2} = ${num1 - num2}`;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-        case "x":
-        case "*":
-            return `${num1} x ${num2} = ${num1 * num2}`;
+function menu() {
+  console.log("");
+  console.log("-- CALCULATOR --");
+  console.log("1 - ADDITION");
+  console.log("2 - SUBTRACTION");
+  console.log("3 - MULTIPLICATION");
+  console.log("4 - DIVISION");
+  console.log("5 - MULTIPLICATION TABLE");
+  console.log("6 - EXIT");
 
-        case "/":
-            if (num2 === 0) {
-                return "Error: Division by zero";
+  rl.question("Choose an option: ", (option) => {
+    switch (option) {
+      case "1":
+        rl.question("Enter the first number: ", (num1) => {
+          rl.question("Enter the second number: ", (num2) => {
+            console.log(`${num1} + ${num2} = ${Number(num1) + Number(num2)}`);
+            menu();
+          });
+        });
+        break;
+
+      case "2":
+        rl.question("Enter the first number: ", (num1) => {
+          rl.question("Enter the second number: ", (num2) => {
+            console.log(`${num1} - ${num2} = ${Number(num1) - Number(num2)}`);
+            menu();
+          });
+        });
+        break;
+
+      case "3":
+        rl.question("Enter the first number: ", (num1) => {
+          rl.question("Enter the second number: ", (num2) => {
+            console.log(`${num1} x ${num2} = ${Number(num1) * Number(num2)}`);
+            menu();
+          });
+        });
+        break;
+
+      case "4":
+        rl.question("Enter the first number: ", (num1) => {
+          rl.question("Enter the second number: ", (num2) => {
+            if (Number(num2) === 0) {
+              console.log("Error: division by zero!");
+            } else {
+              console.log(`${num1} / ${num2} = ${Number(num1) / Number(num2)}`);
             }
-            return `${num1} / ${num2} = ${num1 / num2}`;
+            menu();
+          });
+        });
+        break;
 
-        default:
-            return "Enter a valid option!";
+      case "5":
+        rl.question("Enter a number for the multiplication table: ", (table) => {
+          for (let i = 0; i <= 10; i++) {
+            console.log(`${table} x ${i} = ${Number(table) * i}`);
+          }
+          menu();
+        });
+        break;
+
+      case "6":
+        console.log("Exiting...");
+        rl.close();
+        break;
+
+      default:
+        console.log("Please enter a valid option!");
+        menu();
+        break;
     }
+  });
 }
 
-let result = calculator(); //Enter num1, num2 and an operator.
-console.log(result);
-
+menu();
