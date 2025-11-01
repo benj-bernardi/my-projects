06a0312e34785f169c6cf642cd847c2b9409c15d
benj-bernardi <benjaminbernardi33@gmail.com>
2025-11-01 17:47:40 -0300

    add: JS class study examples

--- javascript-practice/study7.js
+++ javascript-practice/study7.js
@@ -69,3 +69,68 @@ const user = new BankAccount("Benjamin", 1500);
 console.log(user.deposit(50));
 console.log(user.withdraw(150));
 console.log(user.checkBalance());
+
+//-----------------------------------------------------------
+
+class User {
+    constructor(name, password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    authenticate(name, enteredPassword) {
+        if (name !== this.name) {
+            return "User not found!";
+        }
+
+        if (enteredPassword !== this.password) {
+            return "Incorrect password!";
+        }
+
+        return `Password authenticated successfully! Welcome, ${this.name}`;
+    }
+}
+
+// Creating users
+const user1 = new User("John", 1234);
+const user2 = new User("Mary", 4321);
+
+// Testing authentication
+console.log(user1.authenticate("John", 1234));
+console.log(user2.authenticate("Mary", 9999)); 
+
+//------------------------------------------
+
+class Character { 
+    constructor(name, health, attack) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+    }
+}
+
+class Mage extends Character {
+    constructor(name, health, attack, magic) {
+        super(name, health, attack);
+        this.magic = magic;
+    }
+    castSpell() {
+        console.log(`${this.name} used a spell against the Goblin and dealt ${this.attack + this.magic} damage!`);
+    }
+}
+
+class Warrior extends Character {
+    constructor(name, health, attack, strength) {
+        super(name, health, attack);
+        this.strength = strength;
+    }
+    attack() {
+        console.log(`${this.name} attacked the Goblin with a sword and dealt ${this.attack + this.strength} damage!`);
+    }
+}
+
+const newCharacter = new Mage("Merlin", 100, 30, 50);
+const newCharacter2 = new Warrior("Arthur", 150, 50, 40);
+
+newCharacter.castSpell();
+newCharacter2.attack();
