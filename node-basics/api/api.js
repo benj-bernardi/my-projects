56ce69dbe2c118114d92d253a069ac7f8db328a5
benj-bernardi <benjaminbernardi33@gmail.com>
2025-11-09 23:47:33 -0300

    feat: create basic express server

---
+++ node-basics/api/api.js
@@ -0,0 +1,15 @@
+// API REST study
+
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}`);
+});
+
