5408199ab6394246af97b122defec82ed59f3a7b
benj-bernardi <benjaminbernardi33@gmail.com>
2025-11-14 12:43:36 -0300

    feat: add DELETE route to remove users by ID

--- node-basics/api/api.js
+++ node-basics/api/api.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 app.use(express.json());
 
 let users = [];
+let nextID = 1;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -26,7 +27,7 @@ app.post('/users', (req, res) => {
     return res.status(400).json({ error: 'Name and email are required.' });
   }
 
-  const newUser = { id: Date.now(), name, email };
+  const newUser = { id: nextID++, name, email };
   users.push(newUser);
 
   return res.status(201).json(newUser);
@@ -35,4 +36,19 @@ app.post('/users', (req, res) => {
 // List all users:
 app.get('/users', (req, res) => {
   res.status(200).json(users);
+});
+
+// Delete user by id:
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = users.findIndex(user => user.id == id);
+
+  if (index === -1) {
+    return res.status(404).json({ error: "User not found." });
+  }
+
+  users.splice(index, 1);
+
+  res.status(200).json({ message: "User deleted successfully." });
 });
