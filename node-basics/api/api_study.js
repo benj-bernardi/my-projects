e0327af7339b3487b0a124463e7cefef91df83e4
benj-bernardi <benjaminbernardi33@gmail.com>
2026-01-01 21:10:24 -0300

    feat: implement users CRUD with PostgreSQL

--- node-basics/api/api_study.js
+++ node-basics/api/api_study.js
@@ -1,9 +1,9 @@
 // EXPRESS CRUD
 import express from "express";
+import pool from  "./database/db_api_study.js";
 
 const app = express();
 const PORT = 3000;
-const users = [];
 
 app.use(express.json());
 
@@ -15,61 +15,74 @@ app.get("/", (req, res) => {
     res.send("Welcome to the users API.");
 });
 
-app.get("/users", (req, res) => {
-    res.json(users);
+app.get("/users", async (req, res) => {
+    try {
+        const users = await pool.query("SELECT * FROM users");
+        res.status(200).json(users.rows);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "DATABASE error" });
+    }
 });
 
-app.post("/users", (req, res) => {
-    const { name, password } = req.body;
+app.post("/users", async (req, res) => {
+    try {
+        const { name, password } = req.body;
 
-    if (!name || !password) {
-        return res.status(400).json({ error: "Name and password are required." });
+        if (!name || !password){
+            return res.status(400).json({ error: "Name and password are required." });
+        }
+
+        const newUser = await pool.query("INSERT INTO users (name, password) VALUES ($1, $2) RETURNING *", [name, password]);
+
+        res.status(201).json(newUser.rows[0]);
     }
-
-    const newUser = {
-        id: Date.now(),
-        name,
-        password
-    };
-
-    users.push(newUser);
-
-    return res.status(201).json(newUser);
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "DATABASE error" });
+    }
 });
 
-app.put("/users/:id", (req, res) => {
-    const { id } = req.params;
-    const { name, password } = req.body;
+app.put("/users/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, password } = req.body;
 
-    if (!name || !password) {
-        return res.status(400).json({ error: "Name and password are required." });
+        if (!name || !password){
+            return res.status(400).json({ error: "Name and password are required" });
+        }
+
+        const index = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+
+        if (index.rows.length === 0){
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const updateUser = await pool.query("UPDATE users SET name = $1, password = $2 WHERE id = $3 RETURNING *", [name, password, id]);
+
+        res.status(204).send();
     }
-
-    const index = users.findIndex(user => user.id == id);
-
-    if (index === -1) {
-        return res.status(404).json({ error: "User not found." });
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "DATABASE error" });
     }
-
-    users[index] = {
-        id: users[index].id,
-        name,
-        password
-    };
-
-    return res.status(200).json({ message: "User updated successfully.", user: users[index]});
 });
 
-app.delete("/users/:id", (req, res) => {
-    const { id } = req.body;
+app.delete("/users/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        const result = await pool.query("DELETE FROM users WHERE id = $1", [id]);
 
-    const index = users.findIndex(u => u.id == id);
+        if (result.rowCount === 0){
+            return res.status(404).json({ error: "User not found" });
+        }
 
-    if (index === -1){
-        return res.status(400).json({ error: "User not found" });
+        res.status(204).send();
     }
-
-    users.splice(index, 1);
-
-    res.status(200).json({ message: "User deleted successfully" });
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "DATABASE error" });
+    }
 })
